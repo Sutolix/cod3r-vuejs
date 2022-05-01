274231bee01082f58fb809b0625d0be49823a612
Sutolix <tg884ght@gmail.com>
2022-05-01 11:01:03 -0300

    components challenge

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <Sidebar />
+    <Toolbar />
+    <v-content>
+      <Carousel />
+    </v-content>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Sidebar from "./Sidebar.vue";
+import Toolbar from "./Toolbar.vue";
+import Carousel from "./Carousel.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Sidebar,
+    Toolbar,
+    Carousel,
+    Footer,
+  },
+};
+</script>
---
+++ componentes/componente-desafio/src/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-navigation-drawer app>
+    <MenuToolbar />
+    <v-divider></v-divider>
+    <MenuItens />
+  </v-navigation-drawer>
+</template>
+
+<script>
+import MenuToolbar from "./MenuToolbar.vue";
+import MenuItens from "./MenuItens.vue";
+
+export default {
+  name: "app-sidebar",
+  components: {
+    MenuToolbar,
+    MenuItens,
+  },
+};
+</script>
+
+<style>
+</style>
