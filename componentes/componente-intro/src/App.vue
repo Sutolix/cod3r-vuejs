1a5c8e0019a30eff81441d9028f0a5378fbb091a
Sutolix <tg884ght@gmail.com>
2022-05-01 09:16:08 -0300

    create components intro

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <span>{{ counter }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+ 
